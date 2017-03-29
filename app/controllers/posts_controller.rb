425c24ecac3fa03068ac5a6ebe1b3fce2ecf0257
gpdno <gpdno@mac.com>
2017-03-29 13:03:33 -0400

    added post new and create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,23 @@ class PostsController < ApplicationController
   def index
 
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:date, :rationale))
+
+    if @post.save
+      redirect_to post_path(@post), notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
   
 end
